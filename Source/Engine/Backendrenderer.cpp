0c394ff25258369be10cc4a6c36390f7eec5cc6e
Convery <Convery10122@gmail.com>
2018-06-26 14:14:03 +0200

    Added drawing-primitives.

--- Source/Engine/Backendrenderer.cpp
+++ Source/Engine/Backendrenderer.cpp
@@ -97,23 +97,61 @@ namespace Engine
         // Primitives.
         namespace Internal
         {
-            // Fill a polygon.
+            // Draw a line. // TODO, some anti-aliasing.
+            ainline void drawLine(point2_t Start, point2_t Stop, std::function<void(const point2_t Position)> Callback)
+            {
+                // Check if we should invert the line so we always draw left to right.
+                const bool Steep{ std::abs(Start.x - Stop.x) < std::abs(Start.y - Stop.y) };
+                if (Steep)
+                {
+                    std::swap(Start.x, Start.y);
+                    std::swap(Stop.x, Stop.y);
+                }
+                if (Start.x > Stop.x || Start.y > Stop.y) std::swap(Start, Stop);
+
+                // How much to move per iteration.
+                const auto DeltaX{ int16_t(Stop.x - Start.x) };
+                const auto DeltaY{ int16_t(Stop.y - Start.y) };
+
+                // For each X coordinate.
+                int16_t Y{ Start.y }, Error{};
+                const auto Deltaerror{ int16_t(std::abs(DeltaY) * 2) };
+                for (int16_t X = std::max(Start.x, Currentclippingarea.x0); X <= std::min(Stop.x, Currentclippingarea.x1); ++X)
+                {
+                    // Invert the coordinates if too steep.
+                    if (Steep) Callback({ Y, X });
+                    else Callback({ X, Y });
+
+                    // Update the error offset.
+                    Error += Deltaerror;
+                    if (Error > DeltaX)
+                    {
+                        Y += Stop.y > Start.y ? 1 : -1;
+                        Error -= DeltaX * 2;
+
+                        // out of bounds.
+                        if (Y < Currentclippingarea.y0 || Y > Currentclippingarea.y1) break;
+                    }
+                }
+            }
+
+            // Outline and fill polygons. fill -> outline
             ainline void fillPolygon(const vec2_t *Vertices, const size_t Verticecount, std::function<void(const point2_t Position)> Callback)
             {
-                auto Nodes{ std::make_unique<uint16_t[]>(Verticecount + 1) };
+                auto Nodes{ std::make_unique<int16_t[]>(Verticecount + 1) };
 
                 // For each row in the canvas.
-                for (uint16_t Y = Currentclippingarea.y0; Y < Currentclippingarea.y1; ++Y)
+                for (int16_t Y = Currentclippingarea.y0; Y < Currentclippingarea.y1; ++Y)
                 {
-                    uint16_t i{}, j{ uint16_t(Verticecount - 1) }, Nodecount{};
+                    int16_t i{}, j{ int16_t(Verticecount - 1) }, Nodecount{};
 
                     // Create a few nodes.
-                    for (i = 0; i < Verticecount; ++i)
+                    for (i = 0; i < int16_t(Verticecount); ++i)
                     {
                         if ((Vertices[i].y <= Y && Vertices[j].y >= Y) || (Vertices[j].y <= Y && Vertices[i].y >= Y))
                         {
-                            if (Vertices[j].y - Vertices[i].y == 0) Nodes[Nodecount++] = Vertices[i].x + (Y - Vertices[i].y) * (Vertices[j].x - Vertices[i].x);
-                            else Nodes[Nodecount++] = Vertices[i].x + ((Y - Vertices[i].y) / (Vertices[j].y - Vertices[i].y)) * (Vertices[j].x - Vertices[i].x);
+                            if (Vertices[j].y - Vertices[i].y == 0) Nodes[Nodecount++] = int16_t(Vertices[i].x + (Y - Vertices[i].y) * (Vertices[j].x - Vertices[i].x));
+                            else Nodes[Nodecount++] = int16_t(Vertices[i].x + ((Y - Vertices[i].y) / (Vertices[j].y - Vertices[i].y)) * (Vertices[j].x - Vertices[i].x));
                         }
                         j = i;
                     }
@@ -147,50 +185,129 @@ namespace Engine
                     }
                 }
             }
-
-            // Draw a line.
-            ainline void drawLine(point2_t Start, point2_t Stop, std::function<void(const point2_t Position)> Callback)
-            {
-                // Check if we should invert the line so we always draw left to right.
-                const bool Steep{ std::abs(Start.x - Stop.x) < std::abs(Start.y - Stop.y) };
-                if (Steep)
-                {
-                    std::swap(Start.x, Start.y);
-                    std::swap(Stop.x, Stop.y);
-                }
-                if (Start.x > Stop.x || Start.y > Stop.y) std::swap(Start, Stop);
-
-                // How much to move per iteration.
-                const auto DeltaX{ Stop.x - Start.x };
-                const auto DeltaY{ Stop.y - Start.y };
-
-                // For each X coordinate.
-                uint16_t Y{ Start.y }, Error{};
-                const auto Deltaerror{ std::abs(DeltaY) * 2 };
-                for (uint16_t X = std::max(Start.x, Currentclippingarea.x0); X <= std::min(Stop.x, Currentclippingarea.x1); ++X)
-                {
-                    // Invert the coordinates if too steep.
-                    if (Steep) Callback({ Y, X });
-                    else Callback({ X, Y });
-
-                    // Update the error offset.
-                    Error += Deltaerror;
-                    if (Error > DeltaX)
-                    {
-                        Y += Stop.y > Start.y ? 1 : -1;
-                        Error -= DeltaX * 2;
-
-                        // out of bounds.
-                        if (Y < Currentclippingarea.y0 || Y > Currentclippingarea.y1) break;
-                    }
-                }
-            }
-            ainline void drawOutline(const vec2_t *Vertices, const size_t Verticecount, std::function<void(const point2_t Position)> Callback)
+            ainline void outlinePolygon(const vec2_t *Vertices, const size_t Verticecount, std::function<void(const point2_t Position)> Callback)
             {
                 for (size_t i = 0; i < Verticecount - 1; i++)
                 {
-                    drawLine({ uint16_t(Vertices[i].x), uint16_t(Vertices[i].y) }, { uint16_t(Vertices[i + 1].x), uint16_t(Vertices[i + 1].y) }, Callback);
+                    drawLine({ int16_t(Vertices[i].x), int16_t(Vertices[i].y) }, { int16_t(Vertices[i + 1].x), int16_t(Vertices[i + 1].y) }, Callback);
                 }
+            }
+
+            // Outline and fill circles.
+            template <bool Fill = true>
+            ainline void drawCircle(const point2_t Position, const float Radius, std::function<void(const point2_t Position)> Callback)
+            {
+                int16_t X{}, Y{ int16_t(Radius) };
+
+                // Pre-calculate the clipped area.
+                const point4_t Area
+                {
+                    int16_t(std::max((float)Position.x - Radius, (float)Currentclippingarea.x0)),
+                    int16_t(std::max((float)Position.y - Radius, (float)Currentclippingarea.y0)),
+                    int16_t(std::min((float)Position.x + Radius, (float)Currentclippingarea.x1)),
+                    int16_t(std::min((float)Position.y + Radius, (float)Currentclippingarea.y1))
+                };
+
+                // Helpers to keep the algorithm cleaner.
+                auto doCallback = [&Area, &Callback](const point2_t Position) -> void
+                {
+                    // Outside of the clipping area.
+                    if (Position.y < Area.y0 || Position.y > Area.y1) return;
+                    if (Position.x < Area.x0 || Position.x > Area.x1) return;
+                    Callback(Position);
+                };
+                auto doDrawing = [&](const point2_t Origin, const point2_t Size) -> void
+                {
+                    if (Size.x == 0)
+                    {
+                        doCallback({ Origin.x, int16_t(Origin.y + Size.y) });
+                        doCallback({ Origin.x, int16_t(Origin.y - Size.y) });
+
+                        if (Fill)
+                        {
+                            for (int16_t X = int16_t(Origin.x - Size.y); X < int16_t(Origin.x + Size.y); ++X)
+                                doCallback({ X, Origin.y });
+                        }
+                        else
+                        {
+                            doCallback({ int16_t(Origin.x + Size.y), Origin.y });
+                            doCallback({ int16_t(Origin.x - Size.y), Origin.y });
+                        }
+                        return;
+                    }
+
+                    if (Size.x == Size.y)
+                    {
+                        if (Fill)
+                        {
+                            for (int16_t X = int16_t(Origin.x - Size.x); X < int16_t(Origin.x + Size.x); ++X)
+                            {
+                                doCallback({ X, int16_t(Origin.y + Size.y) });
+                                doCallback({ X, int16_t(Origin.y - Size.y) });
+                            }
+                        }
+                        else
+                        {
+                            doCallback({ int16_t(Origin.x + Size.x), int16_t(Origin.y + Size.y) });
+                            doCallback({ int16_t(Origin.x - Size.x), int16_t(Origin.y + Size.y) });
+                            doCallback({ int16_t(Origin.x + Size.x), int16_t(Origin.y - Size.y) });
+                            doCallback({ int16_t(Origin.x - Size.x), int16_t(Origin.y - Size.y) });
+                        }
+                        return;
+                    }
+
+                    if (Size.x < Size.y)
+                    {
+                        if (Fill)
+                        {
+                            for (int16_t X = int16_t(Origin.x - Size.x); X < int16_t(Origin.x + Size.x); ++X)
+                            {
+                                doCallback({ X, int16_t(Origin.y + Size.y) });
+                                doCallback({ X, int16_t(Origin.y - Size.y) });
+                            }
+                            for (int16_t X = int16_t(Origin.x - Size.y); X < int16_t(Origin.x + Size.y); ++X)
+                            {
+                                doCallback({ X, int16_t(Origin.y + Size.x) });
+                                doCallback({ X, int16_t(Origin.y - Size.x) });
+                            }
+                        }
+                        else
+                        {
+                            doCallback({ int16_t(Origin.x + Size.x), int16_t(Origin.y + Size.y) });
+                            doCallback({ int16_t(Origin.x - Size.x), int16_t(Origin.y + Size.y) });
+                            doCallback({ int16_t(Origin.x + Size.x), int16_t(Origin.y - Size.y) });
+                            doCallback({ int16_t(Origin.x - Size.x), int16_t(Origin.y - Size.y) });
+                            doCallback({ int16_t(Origin.x + Size.y), int16_t(Origin.y + Size.x) });
+                            doCallback({ int16_t(Origin.x - Size.y), int16_t(Origin.y + Size.x) });
+                            doCallback({ int16_t(Origin.x + Size.y), int16_t(Origin.y - Size.x) });
+                            doCallback({ int16_t(Origin.x - Size.y), int16_t(Origin.y - Size.x) });
+                        }
+                        return;
+                    }
+                };
+
+                // Draw the cardinals.
+                doDrawing(Position, { X, Y });
+
+                // Draw the rest of the owl.
+                X++; Y = int16_t(std::sqrt(Radius * Radius - 1) + 0.5);
+                while (X < Y)
+                {
+                    doDrawing(Position, { X, Y });
+                    X++; Y = int16_t(std::sqrt(Radius * Radius - X * X) + 0.5);
+                }
+                if (X == Y)
+                {
+                    doDrawing(Position, { X, Y });
+                }
+            }
+            ainline void fillCircle(const point2_t Position, const float Radius, std::function<void(const point2_t Position)> Callback)
+            {
+                return drawCircle<true>(Position, Radius, Callback);
+            }
+            ainline void outlineCircle(const point2_t Position, const float Radius, std::function<void(const point2_t Position)> Callback)
+            {
+                return drawCircle<false>(Position, Radius, Callback);
             }
         }
     }
