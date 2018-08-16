d9c617ccfaf1b4f18d423a5030b9f56ba382994f
Convery <Convery10122@gmail.com>
2018-08-16 13:24:39 +0200

    Added blocks to the sidebar.

--- Source/Menucomponents/Sidebar.cpp
+++ Source/Menucomponents/Sidebar.cpp
@@ -12,6 +12,11 @@
 void Composesidebar(Element_t *Target)
 {
     auto Boundingbox = new Element_t("Sidebar");
+    auto Accountbox = new Element_t("Sidebar.Account");
+    auto Navigationbox = new Element_t("Sidebar.Navbar");
+    auto Launchbox = new Element_t("Sidebar.Quicklaunch");
+
+    // Limit the size to 50px.
     Boundingbox->Properties.Fixedwidth = true;
     Boundingbox->Margins = { 0, 0, 50, 0 };
     Boundingbox->onRender = [](Element_t *Caller)
@@ -19,6 +24,17 @@ void Composesidebar(Element_t *Target)
         Draw::Quad({ 0x22, 0x22, 0x22, 0xFF }, Caller->Dimensions);
     };
     Target->addChild(Boundingbox);
+
+    // Have the account info up top.
+    Accountbox->Properties.Fixedheight = true;
+    Accountbox->Margins = { 0, 0, 0, 70 };
+    Accountbox->onRender = [](Element_t *Caller)
+    {
+        Draw::Circle<>({ 50, 50, 50, 0xFF }, { Caller->Dimensions.x0 + 25, Caller->Dimensions.y0 + 30 }, 22);
+        Draw::Quad<true>({ 50, 50, 50, 0xFF }, { Caller->Dimensions.x0 + 5, Caller->Dimensions.y1, Caller->Dimensions.x1 - 5, Caller->Dimensions.y1 });
+        Draw::Quad<true>({ 0xAA, 0xAA, 0xAA, 0xFF }, { Caller->Dimensions.x0 + 5, Caller->Dimensions.y0 + 56, Caller->Dimensions.x1 - 5, Caller->Dimensions.y1 - 3 });
+    };
+    Boundingbox->addChild(Accountbox);
 }
 
 // Register the composer for later.
