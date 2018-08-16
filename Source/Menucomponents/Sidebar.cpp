e42f3d5689ff5465d25a7a5ba70a7503b70a3d05
Convery <Convery10122@gmail.com>
2018-08-16 15:50:36 +0200

    Added blockout for the sidebar.

--- Source/Menucomponents/Sidebar.cpp
+++ Source/Menucomponents/Sidebar.cpp
@@ -14,6 +14,10 @@ void Composesidebar(Element_t *Target)
     auto Boundingbox = new Element_t("Sidebar");
     auto Accountbox = new Element_t("Sidebar.Account");
     auto Navigationbox = new Element_t("Sidebar.Navbar");
+    auto Homebutton = new Element_t("Navbar.Home");
+    auto Pluginsbutton = new Element_t("Navbar.Plugins");
+    auto Communitybutton = new Element_t("Navbar.Community");
+    auto Leaderboardbutton = new Element_t("Navbar.Leaderboard");
     auto Launchbox = new Element_t("Sidebar.Quicklaunch");
 
     // Limit the size to 50px.
@@ -28,13 +32,60 @@ void Composesidebar(Element_t *Target)
     // Have the account info up top.
     Accountbox->Properties.Fixedheight = true;
     Accountbox->Margins = { 0, 0, 0, 70 };
-    Accountbox->onRender = [](Element_t *Caller)
+    Accountbox->onRender = [&](Element_t *Caller)
     {
-        Draw::Circle<>({ 50, 50, 50, 0xFF }, { Caller->Dimensions.x0 + 25, Caller->Dimensions.y0 + 30 }, 22);
-        Draw::Quad<true>({ 50, 50, 50, 0xFF }, { Caller->Dimensions.x0 + 5, Caller->Dimensions.y1, Caller->Dimensions.x1 - 5, Caller->Dimensions.y1 });
+        // TODO(Convery): Replace with an avatar.
+        Draw::Circle({ 50, 50, 50, 0xFF }, { Caller->Dimensions.x0 + 25, Caller->Dimensions.y0 + 30 }, 22);
+
+        // TODO(Convery): Replace with he user-ID like "#455643".
         Draw::Quad<true>({ 0xAA, 0xAA, 0xAA, 0xFF }, { Caller->Dimensions.x0 + 5, Caller->Dimensions.y0 + 56, Caller->Dimensions.x1 - 5, Caller->Dimensions.y1 - 3 });
     };
     Boundingbox->addChild(Accountbox);
+
+    // Next the navbar.
+    Navigationbox->Properties.Fixedheight = true;
+    Navigationbox->Margins = { 0, 70, 0, 270 };
+    Boundingbox->addChild(Navigationbox);
+
+    // Navigate to the frontpage / library.
+    Homebutton->Properties.Fixedheight = true;
+    Homebutton->Margins = { 0, 0, 0, 50 };
+    Homebutton->onRender = [](Element_t *Caller)
+    {
+        if (Caller->Properties.Hoover) Draw::Quad({ 50, 50, 50, 0xFF }, Caller->Dimensions);
+        /* TODO(Convery): Draw the icon. */
+    };
+    Navigationbox->addChild(Homebutton);
+
+    // Navigate to the plugin 'store'.
+    Pluginsbutton->Properties.Fixedheight = true;
+    Pluginsbutton->Margins = { 0, 50, 0, 100 };
+    Pluginsbutton->onRender = [](Element_t *Caller)
+    {
+        if (Caller->Properties.Hoover) Draw::Quad({ 50, 50, 50, 0xFF }, Caller->Dimensions);
+        /* TODO(Convery): Draw the icon. */
+    };
+    Navigationbox->addChild(Pluginsbutton);
+
+    // Navigate to the social stuffs.
+    Communitybutton->Properties.Fixedheight = true;
+    Communitybutton->Margins = { 0, 100, 0, 150 };
+    Communitybutton->onRender = [](Element_t *Caller)
+    {
+        if (Caller->Properties.Hoover) Draw::Quad({ 50, 50, 50, 0xFF }, Caller->Dimensions);
+        /* TODO(Convery): Draw the icon. */
+    };
+    Navigationbox->addChild(Communitybutton);
+
+    // Navigate to the statistics.
+    Leaderboardbutton->Properties.Fixedheight = true;
+    Leaderboardbutton->Margins = { 0, 150, 0, 200 };
+    Leaderboardbutton->onRender = [](Element_t *Caller)
+    {
+        if(Caller->Properties.Hoover) Draw::Quad({ 50, 50, 50, 0xFF }, Caller->Dimensions);
+        /* TODO(Convery): Draw the icon. */
+    };
+    Navigationbox->addChild(Leaderboardbutton);
 }
 
 // Register the composer for later.
