af22e253a44f7f0b68c761a9fd21a282306308a0
Convery <Convery10122@gmail.com>
2018-08-20 23:51:04 +0200

    Added window dragging.

--- Source/Menucomponents/Toolbar.cpp
+++ Source/Menucomponents/Toolbar.cpp
@@ -11,13 +11,47 @@
 void Composetoolbar(Element_t *Target)
 {
     auto Boundingbox = new Element_t("Toolbar");
+
+
+    // The full toolbar, click to drag.
     Boundingbox->Properties.Fixedheight = true;
     Boundingbox->Margins = { 0, 0, 0, 20 };
     Boundingbox->onRender = [](Element_t *Caller)
     {
         Draw::Quad({ 97, 72, 47, 0xFF }, Caller->Dimensions);
     };
+    Boundingbox->onClicked = [](Element_t *Caller, bool Released)
+    {
+        // Move the window while pressed.
+        static bool isMoving;
+        isMoving = !Released;
+
+        // In a separate thread so we wont block.
+        std::thread([&]() -> void
+        {
+            auto Window{ Engine::gWindowposition };
+            POINT Origin; GetCursorPos(&Origin);
+            POINT Currentposition;
+
+            // While moving.
+            while (isMoving)
+            {
+                GetCursorPos(&Currentposition);
+                Window.x += (Currentposition.x - Origin.x);
+                Window.y += (Currentposition.y - Origin.y);
+                Engine::Window::Move(Window, true);
+                Origin = Currentposition;
+            }
+
+            // Recalculate on exit.
+            Engine::Window::Move(Window);
+        }).detach();
+
+        return true;
+    };
     Target->addChild(Boundingbox);
+
+
 }
 
 // Register the composer for later.
